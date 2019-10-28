b8f2f8b48bded02e6f7967ba5f87d80afe1f8261
Steven Polido <steven.polido@vistage.com>
2019-10-28 13:29:06 -0700

    Updates

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 $(document).foundation();
+
+ 
+$(document).foundation('reveal', {
+    'animation': 'none'
+});
+
+$(document).on('open.fndtn.reveal', '[data-reveal]', function () {
+   var modal = $(this);
+    //$('.reveal-modal-bg').velocity(  {opacity: [ 0.7 ,0]});  
+   var open = $(this).attr('data-easein');
+     if(open == 'shake') {
+                 $(this).velocity('callout.' + open);
+            } else if(open == 'pulse') {
+                 $(this).velocity('callout.' + open);
+            } else if(open == 'tada') {
+               $(this).velocity('callout.' + open);
+            } else if(open == 'flash') {
+                 $(this).velocity('callout.' + open);
+            }  else if(open == 'bounce') {
+                 $(this).velocity('callout.' + open);
+            } else if(open == 'swing') {
+                 $(this).velocity('callout.' + open);
+            }else {
+              $(this).velocity('transition.' + open);
+                     
+            }
+ 
+});
